1db73d76f5e710fe34dd0bc488cb4361838461d9
최희원 <jeeen1@naver.com>
2023-06-05 00:07:29 +0900

    add characters

--- src/constants/idols.ts
+++ src/constants/idols.ts
@@ -37,12 +37,46 @@ export const idolsPhotos: Record<
     {
       photo: "/momo/momo_4.png",
       crop: "momo/momo_4_crop.png",
-      copyright: "햐)",
+      copyright: "햐",
     },
     {
       photo: "/momo/momo_5.png",
       crop: "momo/momo_5_crop.png",
       copyright: "햐",
+    },
+  ],
+  sora: [
+    {
+      photo: "/sora/sora_1.png",
+      crop: "sora/sora_1_crop.png",
+      copyright: "유니",
+    },
+    {
+      photo: "/sora/sora_2.png",
+      crop: "sora/sora_2_crop.png",
+      copyright: "유니",
+    },
+    {
+      photo: "/sora/sora_3.png",
+      crop: "sora/sora_3_crop.png",
+      copyright: "유니",
+    },
+    {
+      photo: "/sora/sora_4.png",
+      crop: "sora/sora_4_crop.png",
+      copyright: "어흥",
+    },
+  ],
+  sena: [
+    {
+      photo: "/sena/sena_1.png",
+      crop: "sena/sena_1_crop.png",
+      copyright: "펭궁",
+    },
+    {
+      photo: "/sena/sena_2.png",
+      crop: "sena/sena_2_crop.png",
+      copyright: "펭궁",
     },
   ],
 };
